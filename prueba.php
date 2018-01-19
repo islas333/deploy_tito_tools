3a76344d760141e02b0f6ddfdcd8b0d68d6afb07
Tito Islas <islas333@gmail.com>
2018-01-19 16:55:42 -0600

    Pruebas con fecha

--- prueba.php
+++ prueba.php
@@ -41,7 +41,7 @@
 	<div class="uk-container deploy-home">
 
 		<br>
-		<h2 class="uk-heading-divider">** Deploy VISOR **</h2>
+		<h2 class="uk-heading-divider">** Deploy VISOR con cambio 16:55**</h2>
 
 		<h3 class="uk-heading-divider">Parametros iniciales</h3>
 		<div class="uk-alert-primary" uk-alert>
